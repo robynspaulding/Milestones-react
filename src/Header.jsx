6a5231be9bca0a851daf2da1c5c647564c013372
RobynSpaulding <robynm27@gmail.com>
2023-03-22 12:01:12 -0700

    installs react bootstrap, utalizes reactbootstrap navbar in header and nav.link in logoutLink

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,7 @@
 import { LogoutLink } from "./LogoutLink";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export function Header() {
   return (
@@ -8,41 +11,20 @@ export function Header() {
         <a href="/kids/new">Add Kid</a> | <a href="/family_trees">My Family Tree</a> | <a href="/kids/new">Add Kid</a>{" "}
         <LogoutLink />
       </nav> */}
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="/">
-          Milestones
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/">
-              Home
-            </a>
-            <a class="nav-item nav-link" href="/login">
-              Login
-            </a>
-            <a class="nav-item nav-link" href="/signup">
-              Signup
-            </a>
-            <a class="nav-item nav-link" href="/family_trees">
-              Family Tree
-            </a>
-            <a class="nav-item nav-link">
-              <LogoutLink />
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Milestones</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Kids</Nav.Link>
+            <Nav.Link href="/kids/new">Add new Kid</Nav.Link>
+            <Nav.Link href="/family_trees">Family Tree</Nav.Link>
+
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <LogoutLink />
+          </Nav>
+        </Container>
+      </Navbar>
     </header>
   );
 }
